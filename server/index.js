5b78c3e5c00e181c3a2dc9fcde2b43764fd80a23
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 13:11:11 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
+import { WorldState } from "./world/worldState.js";
 
+console.log("Athesh World Loaded.");
+console.log("Regions:");
+
+for (const regionId in WorldState.regions) {
+  const region = WorldState.regions[regionId];
+  console.log(`- ${region.name} (${region.type})`);
+}
