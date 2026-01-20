5b73a3d940b7c8801e146c7ded06018aedbc9eff
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 13:46:22 -0800

    Update worldState.js

--- server/world/worldState.js
+++ server/world/worldState.js
@@ -1,9 +1,23 @@
 import { loadRegions } from "./worldLoader.js";
+import { createRegionState } from "./regionStateTemplate.js";
+
+const regions = loadRegions();
+
+const regionStates = {};
+
+for (const regionId in regions) {
+  regionStates[regionId] = createRegionState(regions[regionId]);
+}
 
 export const WorldState = {
-  regions: loadRegions(),
+  regions,
+  regionStates,
 
   getRegion(regionId) {
     return this.regions[regionId] || null;
+  },
+
+  getRegionState(regionId) {
+    return this.regionStates[regionId] || null;
   }
 };
