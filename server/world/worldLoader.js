18821954e2e0c1c1264b4e27e093757a27c9b60c
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 13:08:55 -0800

    Update worldLoader.js

--- server/world/worldLoader.js
+++ server/world/worldLoader.js
@@ -1,1 +1,22 @@
+import fs from "fs";
+import path from "path";
 
+const REGIONS_PATH = new URL("./regions/", import.meta.url);
+
+export function loadRegions() {
+  const regions = {};
+
+  const files = fs.readdirSync(REGIONS_PATH);
+
+  for (const file of files) {
+    if (!file.endsWith(".json")) continue;
+
+    const regionData = JSON.parse(
+      fs.readFileSync(new URL(file, REGIONS_PATH))
+    );
+
+    regions[regionData.id] = regionData;
+  }
+
+  return regions;
+}
