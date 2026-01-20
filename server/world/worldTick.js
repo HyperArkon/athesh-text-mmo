2675f107de7cd38fba888bc2ce3e071538be9939
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 15:47:47 -0800

    Update worldTick.js

--- server/world/worldTick.js
+++ server/world/worldTick.js
@@ -1,11 +1,16 @@
 import { WorldState } from "./worldState.js";
 import { runRegionTick } from "./regionTick.js";
+import { runWorldFactionTick } from "./worldFactionTick.js";
 
 export function runWorldTick() {
+  // Update regions
   for (const regionId in WorldState.regions) {
     const regionData = WorldState.getRegion(regionId);
     const regionState = WorldState.getRegionState(regionId);
 
     runRegionTick(regionData, regionState);
   }
+
+  // Update factions
+  runWorldFactionTick();
 }
