c9a296913545945090e41e327b1c83dbc43922e7
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 15:38:01 -0800

    Update worldState.js

--- server/world/worldState.js
+++ server/world/worldState.js
@@ -1,23 +1,37 @@
 import { loadRegions } from "./worldLoader.js";
+import { loadFactions } from "./factionLoader.js";
 import { createRegionState } from "./regionStateTemplate.js";
+import { createFactionState } from "./factionTemplate.js";
 
 const regions = loadRegions();
+const factions = loadFactions();
 
 const regionStates = {};
+const factionStates = {};
 
 for (const regionId in regions) {
   regionStates[regionId] = createRegionState(regions[regionId]);
 }
 
+for (const factionId in factions) {
+  factionStates[factionId] = createFactionState(factions[factionId]);
+}
+
 export const WorldState = {
   regions,
+  factions,
   regionStates,
+  factionStates,
 
-  getRegion(regionId) {
-    return this.regions[regionId] || null;
+  getRegion(id) {
+    return this.regions[id] || null;
   },
 
-  getRegionState(regionId) {
-    return this.regionStates[regionId] || null;
+  getFaction(id) {
+    return this.factions[id] || null;
+  },
+
+  getFactionState(id) {
+    return this.factionStates[id] || null;
   }
 };
