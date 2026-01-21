d4d08dc7513bdc3f6f30faa8e92031e29f60abbf
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 16:43:37 -0800

    Create playerCrimeActions.js

---
+++ server/world/playerCrimeActions.js
@@ -0,0 +1,38 @@
+export const PlayerCrimeActions = {
+  theft: {
+    severity: 5,
+    heat: 8,
+    regionCrimeImpact: 4,
+    gangAffinity: 0.3
+  },
+  assault: {
+    severity: 10,
+    heat: 15,
+    regionCrimeImpact: 8,
+    gangAffinity: 0.4
+  },
+  murder: {
+    severity: 25,
+    heat: 35,
+    regionCrimeImpact: 20,
+    gangAffinity: 0.6
+  },
+  smuggling: {
+    severity: 12,
+    heat: 10,
+    regionCrimeImpact: 6,
+    gangAffinity: 0.5
+  },
+  extortion: {
+    severity: 15,
+    heat: 18,
+    regionCrimeImpact: 10,
+    gangAffinity: 0.5
+  },
+  assassination: {
+    severity: 40,
+    heat: 60,
+    regionCrimeImpact: 30,
+    gangAffinity: 0.8
+  }
+};
