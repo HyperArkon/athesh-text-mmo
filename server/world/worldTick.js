0caa0c484011d54d59533812e33d37a838f3f0c9
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 16:47:21 -0800

    Update worldTick.js

--- server/world/worldTick.js
+++ server/world/worldTick.js
@@ -3,6 +3,8 @@ import { runRegionTick } from "./regionTick.js";
 import { runWorldFactionTick } from "./worldFactionTick.js";
 import { runCrimeTick } from "./crimeTick.js";
 import { runGangFactionInteractions } from "./gangFactionInteraction.js";
+import { runPlayerCrimeTick } from "./playerCrimeTick.js";
+import { checkGangRecruitment } from "./playerGangRecruitment.js";
 
 export function runWorldTick() {
   // Update regions
@@ -12,6 +14,11 @@ export function runWorldTick() {
   for (const regionId in WorldState.regions) {
   const region = WorldState.getRegion(regionId);
   runCrimeTick(region, WorldState);
+}
+for (const playerId in WorldState.players) {
+  const player = WorldState.players[playerId];
+  runPlayerCrimeTick(player, WorldState);
+  checkGangRecruitment(player, WorldState);
 }
 
 runGangFactionInteractions(WorldState);
