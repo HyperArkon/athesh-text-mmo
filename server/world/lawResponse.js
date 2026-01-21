e03126a282b5af36a6267b3d7dbbf273c566f50f
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 16:52:10 -0800

    Create lawResponse.js

---
+++ server/world/lawResponse.js
@@ -0,0 +1,18 @@
+export function handleLawResponse(player, region) {
+  const law = region.lawEnforcement;
+
+  if (player.crime.wantedLevel < 3) {
+    player.status = "warned";
+    law.alertLevel += 2;
+    return;
+  }
+
+  if (player.crime.wantedLevel < 6) {
+    player.status = "pursued";
+    law.alertLevel += 5;
+    return;
+  }
+
+  player.status = "hunted";
+  law.alertLevel += 10;
+}
