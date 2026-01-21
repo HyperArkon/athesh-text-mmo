4826d1cb7a66170a3272b13628b1c0d3312865de
HyperArkon <zackeryp2345@gmail.com>
2026-01-20 19:34:15 -0800

    Create goods.js

---
+++ server/world/economy/goods.js
@@ -0,0 +1,10 @@
+export const GOODS = {
+  grain: { basePrice: 10, illegal: false },
+  water: { basePrice: 8, illegal: false },
+  iron: { basePrice: 25, illegal: false },
+  wood: { basePrice: 15, illegal: false },
+  spices: { basePrice: 40, illegal: false },
+  silk: { basePrice: 60, illegal: false },
+  relics: { basePrice: 120, illegal: true },
+  narcotics: { basePrice: 90, illegal: true }
+};
