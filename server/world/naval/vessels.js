c88a48dfb39d1f7014d08799a43dd477900f8d18
HyperArkon <zackeryp2345@gmail.com>
2026-01-22 13:50:44 -0800

    Create vessels.js

---
+++ server/world/naval/vessels.js
@@ -0,0 +1,17 @@
+export const VESSELS = {
+  barge: {
+    capacity: 200,
+    cost: 500,
+    riskModifier: -2
+  },
+  merchantShip: {
+    capacity: 500,
+    cost: 1200,
+    riskModifier: -4
+  },
+  warGalley: {
+    capacity: 300,
+    cost: 2000,
+    riskModifier: -8
+  }
+};
